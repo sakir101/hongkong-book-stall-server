b35be5aa0054d3bde424acba45bffe2725fdd402
sakir101 <sakirhossainf99@gmail.com>
2023-07-18 21:16:17 +0600

    Bug fix

--- dist/app/routes/index.js
+++ dist/app/routes/index.js
@@ -4,27 +4,27 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const user_route_1 = require("../modules/users/user.route");
-const cowModel_route_1 = require("../modules/cowModel/cowModel.route");
-const orderHistory_route_1 = require("../modules/orderHistory/orderHistory.route");
+const book_route_1 = require("../modules/book/book.route");
+const wishList_route_1 = require("../modules/wishList/wishList.route");
 const auth_route_1 = require("../modules/auth/auth.route");
+const booklist_route_1 = require("../modules/booklist/booklist.route");
 const router = express_1.default.Router();
 const moduleRoutes = [
+    {
+        path: '/book',
+        route: book_route_1.BookRoutes
+    },
+    {
+        path: '/wishList',
+        route: wishList_route_1.WishLstRoutes
+    },
     {
         path: '/auth',
         route: auth_route_1.AuthRoutes
     },
     {
-        path: '/cows',
-        route: cowModel_route_1.CowModelRoutes
-    },
-    {
-        path: '/orders',
-        route: orderHistory_route_1.OrderHistoryRoutes
-    },
-    {
-        path: '/users',
-        route: user_route_1.UserRoutes
+        path: '/bookList',
+        route: booklist_route_1.BookLstRoutes
     },
 ];
 moduleRoutes.forEach(route => router.use(route.path, route.route));
